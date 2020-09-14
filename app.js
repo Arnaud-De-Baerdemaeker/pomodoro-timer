b18abf58700b8b2d555c71139dd2f5d994c295ce
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-09-14 16:13:05 +0200

    Add the timer countdown

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import StartStopTimer from "./start-stop-timer.js";
 import ResetTimer from "./reset-timer.js";
+import TimerAt25 from "./timer-at-25.js";
 import AddMinutes from "./add-minutes.js";
 import SubMinutes from "./sub-minutes.js";
 import AddSeconds from "./add-seconds.js";
@@ -10,33 +11,84 @@ import SubSeconds from "./sub-seconds.js";
 const Timer = () => {
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
+    const [startStop, setStartStop] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    useEffect(() => {
+        let timer;
+
+        if (startStop) {
+            if (seconds > 0) {
+                timer = setTimeout(() => {
+                    setSeconds(seconds - 1);
+                }, 1000);
+            } else if (seconds === 0 && minutes > 0) {
+                timer = setTimeout(() => {
+                    setSeconds(59);
+                    setMinutes(minutes - 1);
+                }, 1000);
+            } else {
+                clearTimeout(timer);
+                setStartStop(!startStop);
+            }
+        }
+    });
 
     return (
         <div className={"timer"}>
             <h1 className={"timer__title"}>{"Pomodoro Timer"}</h1>
             <div>
                 <div>
+                    {minutes < 10 ? "0" : ""}
                     {minutes}
                     {":"}
+                    {seconds < 10 ? "0" : ""}
                     {seconds}
                 </div>
             </div>
             <div>
-                <StartStopTimer />
+                <StartStopTimer
+                    startStop={startStop}
+                    setStartStop={setStartStop}
+                    isDisabled={isDisabled}
+                    setIsDisabled={setIsDisabled}
+                />
                 <ResetTimer
-                    minutes={minutes}
                     setMinutes={setMinutes}
-                    seconds={seconds}
                     setSeconds={setSeconds}
+                    isDisabled={isDisabled}
                 />
             </div>
             <div>
-                <AddMinutes minutes={minutes} setMinutes={setMinutes} />
-                <SubMinutes minutes={minutes} setMinutes={setMinutes} />
+                <TimerAt25
+                    setMinutes={setMinutes}
+                    setSeconds={setSeconds}
+                    isDisabled={isDisabled}
+                />
             </div>
             <div>
-                <AddSeconds seconds={seconds} setSeconds={setSeconds} />
-                <SubSeconds seconds={seconds} setSeconds={setSeconds} />
+                <AddMinutes
+                    minutes={minutes}
+                    setMinutes={setMinutes}
+                    isDisabled={isDisabled}
+                />
+                <SubMinutes
+                    minutes={minutes}
+                    setMinutes={setMinutes}
+                    isDisabled={isDisabled}
+                />
+            </div>
+            <div>
+                <AddSeconds
+                    seconds={seconds}
+                    setSeconds={setSeconds}
+                    isDisabled={isDisabled}
+                />
+                <SubSeconds
+                    seconds={seconds}
+                    setSeconds={setSeconds}
+                    isDisabled={isDisabled}
+                />
             </div>
         </div>
     );
